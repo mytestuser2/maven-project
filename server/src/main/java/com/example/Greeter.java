1b60819788acd7ec8ac58637278ea08ddb1716c9
Ahmed Ali <ahmed.ali.hamada@sap.com>
2017-08-10 23:33:35 +0200

    fixing stylecheck

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+/**
+* @param someone text for greeting this one
+* @return returns greeting sting
+*/
+  public final  String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
